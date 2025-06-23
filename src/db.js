7d913446ffd2b47937946a16eede71425909a235
gsbarr <gabriel.s.barreiro@gmail.com>
2025-06-23 20:10:44 -0300

    asfsa

--- src/db.js
+++ src/db.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 
 //Sintáxis del URI: USUARIO:CONTRASEÑA@CLUSTER/BASE_DE_DATOS
 //const MONGODB_URI = `mongodb+srv://${process.env.BD_USUARIO}:${process.env.BD_PASSWORD}@${process.env.CLUSTER}.wzmsqah.mongodb.net/${process.env.BD_NOMBRE}?retryWrites=true&w=majority`;
-const MONGODB_URI = `mongodb+srv://acostafacu30:1234@escueladb.d8xoue1.mongodb.net/?retryWrites=true&w=majority&appName=escueladb`;
+const MONGODB_URI = `mongodb+srv://${process.env.BD_USUARIO}:${process.env.BD_PASSWORD}@${process.env.CLUSTER}.d8xoue1.mongodb.net/${process.env.BD_NOMBRE}?retryWrites=true&w=majority&appName=escueladb`;
+
+// URI de la BD del mail radioweb
+//const MONGODB_URI = `mongodb+srv://${process.env.BD_USUARIO}:${process.env.BD_PASSWORD}@${process.env.CLUSTER}.afkzzfe.mongodb.net/${process.env.BD_NOMBRE}?retryWrites=true&w=majority&appName=radioCluster`;
 
 // Conectamos a bd
 mongoose.connect(MONGODB_URI)
